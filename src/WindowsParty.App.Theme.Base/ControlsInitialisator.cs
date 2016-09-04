e349ef166e671b56a5f3f0f180dd502312c2c605
k.blazevicius@new-vision.com <karolis.blazevicius@strongpoint.com>
2016-09-04 22:45:11 +0300

    Infrastructure for theming and ApplicationContentControl.

---
+++ src/WindowsParty.App.Theme.Base/ControlsInitialisator.cs
@@ -0,0 +1,12 @@
+﻿using WindowsParty.App.Components;
+
+namespace WindowsParty.App.Theme.Base
+{
+    public class ControlsInitialisator
+    {
+        public ControlsInitialisator()
+        {
+            var a = new ApplicationContentControl();
+        }
+    }
+}
