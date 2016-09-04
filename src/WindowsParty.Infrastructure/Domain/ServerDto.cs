d61b3b88101f25faf80e4a8cfa21cb36e4d679c3
korulis <broliukas@gmail.com>
2016-09-05 00:52:51 +0300

    ServerDto

---
+++ src/WindowsParty.Infrastructure/Domain/ServerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WindowsParty.Infrastructure.Domain
+{
+    public class ServerDto
+    {
+        public string Server { get; set; }
+        public string Distance { get; set; }
+    }
+}
