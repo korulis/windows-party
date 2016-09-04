015c64a39fc3922463e1a701fea7e3bf74a793c3
k.blazevicius@new-vision.com <karolis.blazevicius@strongpoint.com>
2016-09-04 19:45:25 +0300

    Fixed module registration configuration.

--- src/WindowsParty.App/BootStrapper.cs
+++ src/WindowsParty.App/BootStrapper.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Windows;
+using WindowsParty.Infrastructure;
 using Microsoft.Practices.Unity;
+using Prism.Modularity;
+using Prism.Regions;
 using Prism.Unity;
 
 namespace WindowsParty.App
@@ -10,16 +14,28 @@ namespace WindowsParty.App
         {
             return Container.Resolve<Shell>();
         }
-        protected override void InitializeModules()
+
+        protected override void InitializeShell()
         {
-            base.InitializeModules();
+            base.InitializeShell();
+
             Application.Current.MainWindow = (Shell)this.Shell;
             Application.Current.MainWindow.Show();
         }
-        protected override void ConfigureModuleCatalog()
+
+        protected override void InitializeModules()
         {
-            base.ConfigureModuleCatalog();
-            this.ModuleCatalog.AddModule(null); // placeholder
+            base.InitializeModules();
+
+            var regionManager = Container.Resolve<IRegionManager>();
+            regionManager.RequestNavigate(Regions.MainRegion, new Uri(AppViews.LoginView, UriKind.Relative));
+
+        }
+        protected override IModuleCatalog CreateModuleCatalog()
+        {
+            var catalog = new ModuleCatalog();
+            catalog.AddModule(typeof(MainModule.MainModule));
+            return catalog;
         }
     }
 }
