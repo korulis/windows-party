db480484f7cebdfb78575d7a60deb56c7034e50b
korulis <broliukas@gmail.com>
2016-09-05 00:19:08 +0300

    First UI Acceptance test is passing!

---
+++ src/MainModule/ServersViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using WindowsParty.Infrastructure;
+using WindowsParty.Infrastructure.Navigation;
+using Prism.Commands;
+using Prism.Regions;
+
+namespace MainModule
+{
+    public class ServersViewModel : INotifyPropertyChanged
+    {
+        private readonly INavigator _navigator;
+        public ICommand LogoutCommand { get; }
+
+        public ServersViewModel(INavigator navigator)
+        {
+            _navigator = navigator;
+            LogoutCommand = new DelegateCommand(OnLogout);
+        }
+
+        private void OnLogout()
+        {
+            _navigator.GoTo(AppViews.InitialView);
+        }
+
+
+        public void OnNavigatedTo(NavigationContext navigationContext)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool IsNavigationTarget(NavigationContext navigationContext)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void OnNavigatedFrom(NavigationContext navigationContext)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
